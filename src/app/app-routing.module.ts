fab4c7ab11ce729c2099633dad321df7780e1ece
Sebastian Quiroz <sebastian.122@hotmail.com>
2020-05-08 21:26:43 -0500

    Se configura app-routing, se crea la primera ruta, se modifica la plantilla con <Router-oulet> para que en esa seccion se modifique el cambio y se agrega routerLink en uno de los botones del header para diteccionar a la ruta configurada '/clientes/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
 
-
+const routes = [
+  {
+    path: '',
+    redirectTo: '/clientes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'clientes',
+    component: ClienteComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
