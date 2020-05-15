17b8a872aaa6f840fc654fc88a75ee32e3e1803d
Sebastian Quiroz <sebastian.122@hotmail.com>
2020-05-15 14:14:21 -0500

    Se crea el servico delete() en clienteservice.ts se implementa en el metodo tambien creado delete() ubicado en cliente.component.ts donde le agregamos una alerta con sweetAlert2 antes de confirmar la eliminacion al darle clic en el boton de eliminar creado he implementado en clientes.html

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../models/cliente';
 import { ClienteService } from '../core/cliente.service';
+//SweeteAlert2 api para mostrar alertas profesionales
+import  swal  from "sweetalert2";
 
 @Component({
   selector: 'app-cliente',
@@ -9,15 +11,47 @@ import { ClienteService } from '../core/cliente.service';
 })
 export class ClienteComponent implements OnInit {
 
-  clientes : Cliente[];
+  clientes: Cliente[];
 
   constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
     // Suscribimos la variable para detectar los cambios
     this.clienteService.getClientes().subscribe(
-      datos => this.clientes = datos
+      clientes => this.clientes = clientes
     );
   }
 
+  delete(cliente: Cliente): void {
+    // Se implementa boton de confirmacion con sweetAlert2
+    swal.fire({
+      title: `Quiere eliminar a ${cliente.nombre}?`,
+      text: "Recuerde que al eliminar el cliente no podra recuperlo",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, quiero eliminar!'
+    }).then((result) => {
+      // si la opcion es si se ejecuta el servicio de delete
+      if (result.value) {
+        this.clienteService.delete(cliente.id)
+        .subscribe(
+          response => {
+            // Filtrar los clientes que no han sido eliminados y mostrarlos
+            //para actualizar la tabla
+            this.clientes = this.clientes.filter(
+              cli => cli != cliente
+            );
+            //mensaje de eliminado
+            swal.fire(
+              'Eliminado!',
+              `${cliente.nombre} ha sido eliminado`,
+              'success'
+            )
+          }
+        );
+      }
+    })
+  }
 }
--- src/app/core/cliente.service.ts
+++ src/app/core/cliente.service.ts
@@ -36,4 +36,8 @@ export class ClienteService {
   update(cliente: Cliente): Observable<Cliente>{
     return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeader})
   }
+
+  delete(id: number): Observable<Cliente>{
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`,{headers: this.httpHeader});
+  }
 }
